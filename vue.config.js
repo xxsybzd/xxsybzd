cda9593f95f3e2e0fb7c8073aba5cf03528e7f56
xxybzds <1208847284@qq.com>
2023-05-19 22:40:59 +0800

    删除了vue.config.js里的内容

--- vue.config.js
+++ vue.config.js
@@ -12,14 +12,6 @@ module.exports = {
             '^/api': ''  
           }
         },
-        '/m': {
-          target: 'https://m.kuaikanmanhua.com',//漫画
-          // target:"https://api.zhuishushenqi.com",//小说
-          changeOrigin: true,
-          pathRewrite:{  // 路径重写，
-            '^/api': ''  
-          }
-        },
         },
   }
 }
